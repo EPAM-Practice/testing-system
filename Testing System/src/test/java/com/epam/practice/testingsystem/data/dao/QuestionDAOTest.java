3b85019567c273de6d39683d86d4d74220449d7b
Артем Крылов <kav128@outlook.com>
2020-07-06 20:30:14 +0300

    add more tests

--- Testing System/src/test/java/com/epam/practice/testingsystem/data/dao/QuestionDAOTest.java
+++ Testing System/src/test/java/com/epam/practice/testingsystem/data/dao/QuestionDAOTest.java
@@ -4,35 +4,75 @@ import com.epam.practice.testingsystem.data.dto.Answer;
 import com.epam.practice.testingsystem.data.dto.Question;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class QuestionDAOTest {
 
     @Test
     void add() {
-        QuestionDAO questionDAO = new QuestionDAO();
-        Question question = new Question();
-        question.setQuestion("Test question");
-        question.setScore(10);
-        Answer a1 = new Answer();
-        a1.setAnswer("Answer1");
-        a1.setIsCorrect(false);
-        Answer a2 = new Answer();
-        a2.setAnswer("Answer2");
-        a2.setIsCorrect(true);
-        List<Answer> answers = new ArrayList<>();
-        answers.add(a1);
-        answers.add(a2);
-        question.setAnswers(answers);
+        Answer answer1 = Answer.builder()
+                .answer("Answer1")
+                .isCorrect(false)
+                .build();
+        Answer answer2 = Answer.builder()
+                .answer("Answer2")
+                .isCorrect(true)
+                .build();
 
-        TestDAO testDAO = new TestDAO();
-        com.epam.practice.testingsystem.data.dto.Test test = testDAO.find(1);
-        int i = questionDAO.add(test, question);
+        List<Answer> answers = Arrays.asList(answer1, answer2);
 
-        Question question1 = questionDAO.find(i);
-        assertEquals(question1.getQuestion(), question.getQuestion());
+        Question expectedQuestion = Question.builder()
+                .question("Test question")
+                .score(10)
+                .answers(answers)
+                .build();
+
+        com.epam.practice.testingsystem.data.dto.Test test = DAOFactory.getTestDAO().find(5);
+        IQuestionDAO questionDAO = DAOFactory.getQuestionDAO();
+        int index = questionDAO.add(test, expectedQuestion);
+
+        Question actualQuestion = questionDAO.find(index);
+        assertEquals(expectedQuestion.getQuestion(), actualQuestion.getQuestion());
     }
+
+    @Test
+    void find() {
+        Question question = DAOFactory.getQuestionDAO().find(7);
+        int size = question.getAnswers().size();
+        assertEquals(2, size);
+    }
+
+    @Test
+    void findAll() {
+        List<Question> questions = DAOFactory.getQuestionDAO().findAll(1);
+        assertEquals(4, questions.size());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Question> questions = DAOFactory.getQuestionDAO().findAll(DAOFactory.getTestDAO().find(1));
+        assertEquals(4, questions.size());
+    }
+
+    @Test
+    void update() {
+        IQuestionDAO questionDAO = DAOFactory.getQuestionDAO();
+        Question expectedQuestion = questionDAO.find(10);
+        expectedQuestion.setQuestion("Test");
+        questionDAO.update(expectedQuestion);
+        Question actualQuestion = questionDAO.find(10);
+        assertEquals(expectedQuestion.getQuestion(), actualQuestion.getQuestion());
+    }
+
+    @Test
+    void delete() {
+        IQuestionDAO questionDAO = DAOFactory.getQuestionDAO();
+        questionDAO.delete(22);
+        assertNull(questionDAO.find(22));
+    }
+
 }
